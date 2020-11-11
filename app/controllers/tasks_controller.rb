44265c4e11f2fd8551fb9dbb5bca6841b020d8d5
Francisco Aguilar <fiaguilars@gmail.com>
2020-11-11 16:14:14 -0500

    CRUD , B483

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,37 @@
+class TasksController < ApplicationController
+    def index
+        @tasks = Task.all
+    end
+
+    def show
+        @task = Task.find(params[:id])
+    end
+
+    def new
+        @task = Task.new
+    end
+
+    def create
+        @task = Task.new(task_params)
+        if @task.save
+            redirect_to task_path(@task)
+        else
+            render :new
+        end
+    end
+
+    def edit
+        @task = Task.find(params[:id])
+    end
+
+    def update
+        @task = Task.find(params[:id])
+        @task.update(task_params)
+        redirect_to task_path(@task)
+    end
+
+    private
+    def task_params
+        params.require(:task).permit(:name, :details, :completed)
+    end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,28 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+#   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+#   get 'tasks', to: "tasks#index" # Show all
+
+#  #Create, two step process
+#  post 'tasks', to: "tasks#create" # POST THE FORM
+
+#  get 'task/new', to: "tasks#new" # GET rest form = 1 Req.
+  
+
+#  #############################################
+
+#   get 'task/:id', to: "tasks#show", as: :task # Show just one
+
+#   ############################################
+
+  
+#   #Update, two step process 
+#   get "tasks/:id/edit", to: "tasks#edit", as: :edit_task # GET form
+
+#   patch "tasks/:id", to: "tasks#update" # TASKS
+
+#   #############################################
+
+#   delete "tasks/:id", to: "tasks#destroy"
+
+  resources :tasks
 end
